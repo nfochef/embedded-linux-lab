f50a69135ebfde8e827de07f60c7353a61067e36
nfochef <208340097+nfochef@users.noreply.github.com>
2026-05-02 09:06:35 +0200

    egen Buildroot-package recipe hello installerad i image

---
+++ fas-02-buildroot/package-hello/hello.mk
@@ -0,0 +1,20 @@
+################################################################################
+#
+# hello
+#
+################################################################################
+
+HELLO_VERSION = 1.0
+HELLO_SITE = ./package/hello/src
+HELLO_SITE_METHOD = local
+HELLO_LICENSE = MIT
+
+define HELLO_BUILD_CMDS
+	$(MAKE) CC="$(TARGET_CC)" -C $(@D)
+endef
+
+define HELLO_INSTALL_TARGET_CMDS
+	$(MAKE) -C $(@D) install DESTDIR=$(TARGET_DIR)
+endef
+
+$(eval $(generic-package))
